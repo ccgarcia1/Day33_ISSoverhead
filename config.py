05d2854e780405fe36dac6df4cf85020debb8c82
ccgarcia1 <42050096+ccgarcia1@users.noreply.github.com>
2024-02-28 12:52:23 -0300

    Implemented the logic to send email when ISS is near

---
+++ config.py
@@ -0,0 +1,4 @@
+email_from = "yoursender@gmail.com"
+email_receiver = "yourreceiver@gmail.com"
+password_sender = "xxxx xxxx xxxx xxxx"
+subject = "Look at Sky! ISS is near from you"
--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import requests
 from datetime import datetime
+import smtplib
+import config
 
-MY_LAT = -29.6604 # Your latitude
-MY_LONG = -51.0562 # Your longitude
+MY_LAT = -29.6604  # Your latitude
+MY_LONG = -51.0562  # Your longitude
 
-def is_ISS_near():
+
+def is_iss_near():
     response = requests.get(url="http://api.open-notify.org/iss-now.json")
     response.raise_for_status()
-    #data = response.json()
-    ISS_lat = float(response.json()["iss_position"]["latitude"])
-    ISS_long = float(response.json()["iss_position"]["longitude"])
+    # data = response.json()
+    iss_lat = float(response.json()["iss_position"]["latitude"])
+    iss_long = float(response.json()["iss_position"]["longitude"])
 
-    print(f"MY_LAT {MY_LAT + 3} ~ {MY_LAT - 3}" )
-    print(f"MY_LONG {MY_LONG + 3} ~ {MY_LONG - 3}" )
-    print(f"ISS DATA {ISS_lat} {ISS_long}")
-    #ISS_lat = -30
-    #ISS_long = -48
-    #Your position is within +5 or -5 degrees of the ISS position.
-    if (ISS_lat <= MY_LAT + 3 or ISS_lat <= MY_LAT - 3 ) and (ISS_long <= MY_LONG + 3 or ISS_long <= MY_LONG - 3 ):
+    print(f"MY_LAT {MY_LAT + 3} ~ {MY_LAT - 3}")
+    print(f"MY_LONG {MY_LONG + 3} ~ {MY_LONG - 3}")
+    print(f"iss DATA {iss_lat} {iss_long}")
+    # iss_lat = -30
+    # iss_long = -48
+    # Your position is within +5 or -5 degrees of the iss position.
+    if (iss_lat <= MY_LAT + 3 or iss_lat <= MY_LAT - 3) and (iss_long <= MY_LONG + 3 or iss_long <= MY_LONG - 3):
         return True
     return False
 
@@ -38,23 +41,31 @@ def is_dark():
     print(f"{sunrise} {sunset}")
     current_hour = datetime.now().hour
     print(current_hour)
-    #current_hour = 20
+    # current_hour = 20
 
     if sunrise <= current_hour <= sunset:
-        return False # It's daytime
-    return True # It's nighttime
+        return False  # It's daytime
+    return True  # It's nighttime
 
 
-print(is_ISS_near())
+def send_email():
+    msg = f"ISS is near from you"
+    connection = smtplib.SMTP("smtp.gmail.com")
+    fmt = 'From: {}\r\nTo: {}\r\nSubject: {}\r\n\r\n{}'
+    connection.starttls()
+    connection.login(user=config.email_sender, password=config.password_sender)
+    connection.sendmail(from_addr=config.email_sender, to_addrs=config.email_receiver, msg=fmt.format
+                        (config.email_receiver, config.email_sender, config.subject, msg).encode('utf-8'))
+    connection.close()
+
+
+print(is_iss_near())
 print(is_dark())
 
-#If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
+# If the iss is close to my current position
+# ,and it is currently dark
+# Then email me to tell me to look up.
 # BONUS: run the code every 60 seconds.
 
-if is_ISS_near() and is_dark():
-    pass # build the logic to send an email
-
-
-
+if is_iss_near() and is_dark():
+    send_email()
